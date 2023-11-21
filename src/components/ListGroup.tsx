0c7419b7670125166d7c6bd2bf08b1f8aabc8f14
TajAlasfiyaa Ishag <tajalasfiyaa@gmail.com>
2023-11-20 20:12:25 -0800

    inial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,14 @@
+export function ListGroup() {
+    const taj = []
+    return (
+        <>
+            <h1>List Group</h1>
+            <ol className="list-group">
+                <li className="list-group-item">TajAlasfiyaa</li>
+                <li className="list-group-item">i</li>
+                <li className="list-group-item">adf</li>
+                <li className="list-group-item active ">sdaf </li>
+            </ol>
+        </>
+    );
+}
