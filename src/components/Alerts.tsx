f5a068c13b3e7721705ebe7c6333ebc18d4972d7
TajAlasfiyaa Ishag <tajalasfiyaa@gmail.com>
2023-11-22 13:47:18 -0800

    test workflows

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,12 +1,14 @@
 interface Props {
   message: string;
   type?: string;
+  show: boolean
 }
-function Alerts({ message, type = "primary" }: Props) {
+function Alerts({ message, type = "primary" , show }: Props) {
   return (
     <>
-      <div className={"alert alert-" + type} role="alert">
+      <div className={"alert  alert-dismissible fade show alert-" + type} role="alert">
         {message}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
     </>
   );
